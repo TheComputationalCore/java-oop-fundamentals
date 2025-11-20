616dfb0553c717abb8c01c9b28baf12b3f1dbbe4
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-21 02:38:45 +0530

    Update Person_2.java

--- src/Person_2.java
+++ src/Person_2.java
@@ -1,29 +1,30 @@
 package task2;
+
 // Question 1.4
-// creating base class person with attributes name and age
-public class Person_2{
+// Creating base class Person_2 with attributes name and age
+public class Person_2 {
     String name;
     int age;
 
-// creating parameterised constructor for person class
-    public Person(String name, int age) {
+    // Correct constructor name (must match class name)
+    public Person_2(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public static void main(String[] args){
-        // creating an object
-        Employee e = new Employee("tony", 24,11000000,1234567);
+    public static void main(String[] args) {
+        // Creating an object
+        Employee e = new Employee("tony", 24, 11000000, 1234567);
         System.out.println(e);
-
-        }
     }
+}
 
-
-class Employee extends Person{
+// Updated Employee to extend Person_2
+class Employee extends Person_2 {
     int employeeID;
     float salary;
-// using super keyword to initialise the attributes of parent class
+
+    // Using super keyword to initialize the attributes of parent class
     public Employee(String name, int age, float salary, int employeeID) {
         super(name, age);
         this.salary = salary;
@@ -40,6 +41,3 @@ class Employee extends Person{
                 '}';
     }
 }
-
-
-
